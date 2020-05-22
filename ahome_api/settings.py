d2c28d377d1b9db9b6e100411dc0ab56f606506b
hirepanpalomares <hirepan.49@gmail.com>
2020-05-22 00:02:40 -0500

    wqwassassssssssw

--- ahome_api/settings.py
+++ ahome_api/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_hosts.middleware.HostsResponseMiddleware',
+    #'django_hosts.middleware.HostsResponseMiddleware',
     #'corsheaders.middleware.CorsMiddleware',
 ]
 
